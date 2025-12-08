d9656b802618cdc8ed47f154c4d71d5504330648
FaresEdris <faresedres123@gmail.com>
2025-12-08 12:24:04 +0200

    added base and index html files

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Author,Language,Genre,Book,BookInstance
 
-# Create your views here.
+def index(request):
+    authors_num = Author.objects.count()
+    books_num = Book.objects.count()
+    books_instance = BookInstance.objects.count()
+    avail_book_instance = BookInstance.objects.filter(status__exact="a").count()
+    
+    context = {
+        'num_books': books_num,
+        'num_instances': books_instance,
+        'num_instances_available': avail_book_instance,
+        'num_authors': authors_num,
+    }
+    return render(request,"catalog/index.html",context)
